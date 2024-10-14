1b7b13cdbdf3d618625a388440182a45507111e8
GwangeunDev <167871372+GwangeunDev@users.noreply.github.com>
2024-10-14 22:02:36 +0900

    Add files via upload

---
+++ serverB.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = 3001;
+
+app.get('/reactB', (req, res) => {
+    res.send('B');
+});
+
+app.get('/echoA', async (req, res) => {
+    try {
+        const response = await axios.get('http://localhost:3000/reactA');
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).send('Service A 호출 실패');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Service B는 ${PORT} 포트에서 실행 중입니다.`);
+});
